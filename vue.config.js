4160583dd4a1b90270bb8d39555c2b75e4522d10
Mohamed Nashaat <mohammed30816894@f-eng.tanta.edu.eg>
2021-04-27 02:22:14 +0200

    try something with webpack

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  // productionSourceMap: false, // test production option outside the function 
+  productionSourceMap: false,  // test production option outside the function 
   configureWebpack: config => {
-    if (process.env.NODE_ENV === 'production') {
-      // mutate config for production...
-      productionSourceMap = false
-    } else {
-      // mutate for development...
-    }
+    // if (process.env.NODE_ENV === 'production') {
+    //   // mutate config for production...
+    //   productionSourceMap = false 
+    // } else {
+    //   // mutate for development...
+    // }
   },
   transpileDependencies: [
     'vuetify'
